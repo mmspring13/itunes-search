c87fa92a21487bd187090ffe57f9ae2837457738
qq <153030518+mmspring13@users.noreply.github.com>
2025-01-22 02:50:54 +0300

    Update tailwind.config.ts
    cleanup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,42 +3,8 @@
 import { nextui } from '@nextui-org/react';
 import animate from 'tailwindcss-animate';
 import type { Config } from 'tailwindcss';
-// import baseConfig from 'tailwindcss/defaultConfig';
 
 export default {
-  // theme: {
-  //   colors: {
-  //     'blue': '#1fb6ff',
-  //     'purple': '#7e5bef',
-  //     'pink': '#ff49db',
-  //     'orange': '#ff7849',
-  //     'green': '#13ce66',
-  //     'yellow': '#ffc82c',
-  //     'gray-dark': '#273444',
-  //     'gray': '#8492a6',
-  //     'gray-light': '#d3dce6',
-  //   },
-  //   fontFamily: {
-  //     sans: ['Graphik', 'sans-serif'],
-  //     serif: ['Merriweather', 'serif'],
-  //   },
-  //   extend: {
-  //     screens: {
-  //       'sm': {'min': '640px'},
-  //       'md': {'min': '768px'},
-  //       'lg': {'min': '1024px'},
-  //       'xl': {'min': '1280px'},
-  //       '2xl': {'min': '1536px'},
-  //     },
-  //     spacing: {
-  //       '8xl': '96rem',
-  //       '9xl': '128rem',
-  //     },
-  //     borderRadius: {
-  //       '4xl': '2rem',
-  //     }
-  //   }
-  // },
   darkMode: 'class',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
